a988b26aaa487f782a62c34f06f2a2d1bff82609
malek12020 <malekjoseph2020@gmail.com>
2023-02-05 14:31:56 +0200

    worked on variables annotations

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -1,2 +1,20 @@
-// How we define annotations is TS
+// How we define annotations is TS for numbers
 const apples: number = 5;
+// How we define annotations is TS for strings
+const favFruit: string = 'apple';
+// How we define annotations is TS for null
+const nothing: null = null;
+// How we define annotations is TS for undefined
+const nothingElse: undefined = undefined;
+// How we define annotations is TS for array of strings
+const colors: string[] = ['red', 'green', 'blue'];
+// How we define annotations is TS for array of numbers
+const myNumbers: number[] = [1,2]
+// How we define annotations is TS for array of booleans
+const myBoleans: boolean[] = [true, false]
+// How we define annotations is TS for array of booleans
+
+
+
+
+
